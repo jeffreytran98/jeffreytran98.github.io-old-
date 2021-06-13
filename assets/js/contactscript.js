b0b4a31224d22f230bae050e6486ba4be3f28e8a
Jeffrey <jeffreytran98@gmail.com>
2021-06-12 17:55:56 -0700

    bare bones it's working wooo

--- assets/js/contactscript.js
+++ assets/js/contactscript.js
@@ -21,22 +21,5 @@ class LikeButton extends React.Component {
   }
 }
 
-window.onload = function() {
-    document.getElementById('contact-form').addEventListener('submit', function(event) {
-        event.preventDefault();
-        // generate a five digit number for the contact_number variable
-        this.contact_number.value = Math.random() * 100000 | 0;
-        // these IDs from the previous steps
-        emailjs.sendForm('contact_service', 'contact_form', this)
-            .then(function() {
-                console.log('SUCCESS!');
-            }, function(error) {
-                console.log('FAILED...', error);
-            });
-    });
-}
-
-
-emailjs.init('user_l2Q0UI7G2nmk3uNjOzE1h');
 const domContainer = document.querySelector('#like_button_container');
 ReactDOM.render(e(LikeButton), domContainer);
